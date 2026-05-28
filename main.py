68d079555d5dfa4aba15e06bcdfe0fbbdfc01645
volunteer1985 <80355284+volunteer1985@users.noreply.github.com>
2026-05-28 23:12:26 +0300

    finish implementation

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
+
+while is_on:
+    user_order = input(f"What would you like to order? Enter {menu.get_items()} ")
+    if user_order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif user_order == "off":
+        is_on = False
+    else:
+        ordered_item = menu.find_drink(user_order)
+        if coffee_maker.is_resource_sufficient(ordered_item) and money_machine.make_payment(ordered_item.cost):
+            coffee_maker.make_coffee(ordered_item)
+
+
+
